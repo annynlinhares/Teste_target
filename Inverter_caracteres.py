b0255653a6b1fa12f21a094aa7da66aa8f11941c
Anny N. Linhares <111099726+annynlinhares@users.noreply.github.com>
2024-04-05 19:40:31 -0300

    Create Inverter_caracteres.py

---
+++ Inverter_caracteres.py
@@ -0,0 +1,7 @@
+def inverteString(s):
+    return s[::-1]  #Retorna a string invertida com slicing
+
+#String p inverter
+stringOriginal = "Meu nome é Anny!"
+stringInvertida = inverteString(stringOriginal)
+print(stringInvertida)
